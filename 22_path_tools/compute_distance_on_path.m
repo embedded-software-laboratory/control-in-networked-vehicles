431f8fa36177939affe914308ce822a9373e5d9e
Celina Kalus <celina.kalus@rwth-aachen.de>
2020-12-10 15:06:30 +0100

    Added compute_distance_on_path

--- 22_path_tools/compute_distance_on_path.m
+++ 22_path_tools/compute_distance_on_path.m
@@ -4,7 +4,40 @@ function distance = compute_distance_on_path(position, path)
 %   Output is in the range of [s_min, s_max] of the path
 
     % Find closest point on path
+    ds = 0.01;
+    %assert(size(path) > 1);
     
+    i_path_point = 0;
+    s_max = path(end).s;
+    min_dist = 1e300;
+    s_query = 0;
+    s_result = 0;
+    
+    while s_query < s_max
+        s_query = s_query + ds;
+        %assert(i_path_point < size(path) - 1);
+        if s_query > path(i_path_point+1).s
+            i_path_point = i_path_point + 1;
+        end
+        
+        if i_path_point >= size(path)
+            break
+        end
+        
+        % calculate distance to reference path
+        interp = path_interpolation(s_query, path(i_path_point), path(i_path_point + 1));
+        
+        dx = position.x - interp.position_x;
+        dy = position.y - interp.position_y;
+        dist = sqrt(dx*dx + dy*dy);
+        
+        if dist < min_dist
+            min_dist = dist;
+            s_result = s_query;
+        end
+    end
+    
+    distance = s_result;
     % Compute distance
 end
 
