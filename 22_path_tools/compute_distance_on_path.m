c7ff8223bc1a9697433c60aa9c5cfe2215a667ed
Celina Kalus <celina.kalus@rwth-aachen.de>
2021-01-14 16:26:32 +0100

    Fixed errors in compute_distance_on_path, now working properly

--- 22_path_tools/compute_distance_on_path.m
+++ 22_path_tools/compute_distance_on_path.m
@@ -1,4 +1,4 @@
-function distance = compute_distance_on_path(position, path)
+function distance = compute_distance_on_path(position, path_points)
 %COMPUTE_DISTANCE_ON_PATH Computes the distance along the path PATH
 %   Input is a POSITION [x, y] and a PATH
 %   Output is in the range of [s_min, s_max] of the path
@@ -7,34 +7,35 @@ function distance = compute_distance_on_path(position, path)
     ds = 0.01;
     %assert(size(path) > 1);
     
-    i_path_point = 0;
-    s_max = path(end).s;
+    i_path_point = 1;
+    s_max = path_points(end).s;
     min_dist = 1e300;
     s_query = 0;
     s_result = 0;
     
     while s_query < s_max
-        s_query = s_query + ds;
         %assert(i_path_point < size(path) - 1);
-        if s_query > path(i_path_point+1).s
+        if s_query > path_points(i_path_point+1).s
             i_path_point = i_path_point + 1;
         end
         
-        if i_path_point >= size(path)
+        if i_path_point >= size(path_points)
             break
         end
         
         % calculate distance to reference path
-        interp = path_interpolation(s_query, path(i_path_point), path(i_path_point + 1));
+        interp = path_interpolation(s_query, path_points(i_path_point), path_points(i_path_point + 1));
         
-        dx = position.x - interp.position_x;
-        dy = position.y - interp.position_y;
+        dx = position(1) - interp.position_x;
+        dy = position(2) - interp.position_y;
         dist = sqrt(dx*dx + dy*dy);
         
         if dist < min_dist
             min_dist = dist;
             s_result = s_query;
         end
+        
+        s_query = s_query + ds;
     end
     
     distance = s_result;
