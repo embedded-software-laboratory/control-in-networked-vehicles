9911795dd95384ea2b2ea6ebf9877ce2d9a6fb80
Patrick Scheffe <patrick.scheffe@rwth-aachen.de>
2021-03-16 23:52:32 +0100

    plot platooning

--- common/compute_distance_on_path.m
+++ common/compute_distance_on_path.m
@@ -1,43 +1,68 @@
-function [s_k_on_loop] = compute_distance_on_path(position, path_points)
+function s_k_on_loop = compute_distance_on_path(position, path_points)
 %COMPUTE_DISTANCE_ON_PATH Computes the distance along the path PATH_POINTS
-%   Input is a POSITION [x, y] and a path PATH_POINTS
-%   Output is a distance in the range of [0, s_max]
+%   Input is a POSITION [x, y], a path PATH_POINTS, and a current distance S_IN
+%   Output is a distance in the range of [0, Inf]
 
     assert(numel(path_points) > 1);
 
-    ds = 0.01;
-    i_path_point = 1;
-    s_max = path_points(end).s;
-    min_dist = 1e300;
-    s_query = 0;
-    s_k_on_loop = 0;
+    num_subdivs = 4;
 
-    while s_query < s_max
-        % Increase path point index if past a path point
-        if (s_query > path_points(i_path_point+1).s)
-            i_path_point = i_path_point + 1;
+    n_points = numel(path_points);
+    is = 1:n_points-1;
+    
+    s_a = arrayfun(@(i) path_points(i).s, is);
+    s_b = arrayfun(@(i) path_points(i).s, is + 1);
+    
+    for iterations=1:5
+        % create subsegments
+        
+        new_is = ones(numel(is) * num_subdivs,1);
+        new_sa = ones(numel(is) * num_subdivs,1);
+        new_sb = ones(numel(is) * num_subdivs,1);
+        
+        for i=1:numel(is)
+            delta_s = (s_b(i) - s_a(i)) / num_subdivs;
+            s_base = s_a(i);
+            for j=1:num_subdivs
+                new_i = (i-1) * num_subdivs + j;
+                new_is(new_i) = is(i);
+                new_sa(new_i) = s_base;
+                s_base = s_base + delta_s;
+                new_sb(new_i) = s_base;
+            end
         end
         
-        % Break loop if behind last path point
-        if (i_path_point >= size(path_points))
-            break
-        end
         
-        % calculate distance to reference path on current path segment
-        interpolation = path_interpolation(s_query, path_points(i_path_point), path_points(i_path_point + 1));
         
-        dx = position(1) - interpolation.position_x;
-        dy = position(2) - interpolation.position_y;
-        dist_to_path = sqrt(dx*dx + dy*dy);
+        is = new_is;
+        s_a = new_sa;
+        s_b = new_sb;
+
+        s_query = (s_b + s_a) / 2;
+        [dx, dy] = arrayfun(@(i, sq) compute_distance_on_segment(position, sq, path_points(i), path_points(i+1)), is, s_query);
+        distance_squared = dx .* dx + dy .* dy;
         
-        % update minimal distance and corresponding distance along path
-        if (dist_to_path < min_dist)
-            min_dist = dist_to_path;
-            s_k_on_loop = s_query;
-        end
-        
-        % increase path query distance
-        s_query = s_query + ds;
+        numrows = numel(distance_squared);
+        localmin = islocalmin([inf, distance_squared.', inf]);
+        localmin = localmin(2:numrows+1);
+
+        is = is(localmin);
+        s_a = s_a(localmin);
+        s_b = s_b(localmin);
     end
+    
+    distance_squared = distance_squared(localmin);
+
+    s_query = (s_a+s_b)/2;
+    
+    sorted = sortrows(table(s_query,distance_squared),2);
+    s_k_on_loop = sorted{1,1};
 end
 
+function [dx, dy] = compute_distance_on_segment(position, s_query, point_a, point_b)
+    interpolation = path_interpolation(s_query, point_a, point_b);
+    
+    dx = position(1) - interpolation.position_x;
+    dy = position(2) - interpolation.position_y;
+    
+end
